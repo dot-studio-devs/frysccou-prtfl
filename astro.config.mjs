3365eb0cf9867a026febb0bfcfd7dd87c97134ac
frysccou <franespindola71@gmail.com>
2026-02-13 14:30:46 -0300

    feat: establish base layout with comprehensive SEO, theme management, and configure site URL in Astro settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  site: "https://frysccou.dev",
   output: "static",
   adapter: vercel({
     webAnalytics: {
