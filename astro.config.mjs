c595922c0437bde39740ac8d79290f8c888d2adc
frysccou <franespindola71@gmail.com>
2026-02-22 16:20:22 -0300

    feat: add initial About, Hero, and Sidebar components, an image asset, and update Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,29 @@ import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://frysccou-portfolio.vercel.app",
   integrations: [react()],
   adapter: vercel(),
   output: "server",
   image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.twimg.com",
+      },
+    ],
   },
   vite: {
     plugins: [tailwindcss()],
