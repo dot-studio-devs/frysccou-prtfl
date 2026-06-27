21ddb13fd3871acbd7e648840b07e5f1b8dd8e51
frysccou <franespindola71@gmail.com>
2026-06-27 04:46:12 +0000

    Agregado frontend bro

--- src/lib/motion/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./system";
-export * from "./runtime";
-export * from "./intersectionReveal";
-export * from "./patterns";
--- src/lib/turso.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client/web";
-
-const turso = createClient({
-  url: import.meta.env.TURSO!,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN!,
-});
-
-export default turso;
